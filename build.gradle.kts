780a47266695501e3dc514964f76f90f9f75eea2
Carlos Genuino <carloxsgenuino3@live.com>
2025-06-02 14:12:06 -0300

    feat: comment manager

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,6 +26,8 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.apache.commons:commons-lang3")
+	implementation("com.fasterxml.uuid:java-uuid-generator")
 	compileOnly("org.projectlombok:lombok")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	runtimeOnly("com.h2database:h2")
