95e7b645cc1d6bf81ace238a3afa806c7c66aa5f
Carlos Genuino <carloxsgenuino3@live.com>
2025-06-02 09:13:16 -0300

    Adding new project files

---
+++ src/main/java/com/algaworks/comments_moderation/CommentsModerationApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.comments_moderation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommentsModerationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommentsModerationApplication.class, args);
+	}
+
+}
